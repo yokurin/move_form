c3907f460b98ec29f6592c8bfdfc256e4dde61ed
TsubasaHayashi <yoku.rin.99@gmail.com>
2015-02-18 03:19:26 +0900

    静的にはチャットぽくなった

--- sp_form/www/js/app.js
+++ sp_form/www/js/app.js
@@ -4,14 +4,6 @@
 
   module.controller('AppController', function($scope, $data) {
 
-
-
-
-  });
-
-  module.controller('FormController', function($scope, $data) {
-    //
-
   });
 
   module.controller('HomeController', function($scope, $data) {
@@ -34,6 +26,56 @@
     };
 
   });
+
+
+  module.controller('FormController', function($scope, $data) {
+
+    $scope.items = [
+        {
+            question: '名前は？'
+            //answer: 'テスト'
+        }/*,
+        {
+            question: '性別は？',
+            answer: '男'
+        },
+        {
+            question: '誕生日は？',
+            answer: '2015/2/20'
+        },
+        {
+            question: '出身は？',
+            answer: '東京'
+        }*/
+    ];
+
+    $data.num = 0;
+
+
+
+    $scope.postText = function(num)
+    {
+      if(this.inputText)
+      {
+        //get input box value
+        var textValue = this.inputText;
+        console.log("入力内容:"+ textValue);
+
+        $('question_Box').css('display','block');
+
+        $scope.items[num] = ({ question: '名前は？' , answer: this.inputText });
+
+        $scope.items.push({ question: '性別は？' });
+        $scope.inputText = "";　//ボックスのクリア
+        $scope.$apply();   //画面の更新
+      }else{
+        alert("文字が入力されていません。");
+      }
+
+    };
+
+  });
+
 
   module.factory('$data', function() {
       var data = {};
