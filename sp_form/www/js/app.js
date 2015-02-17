dc3fc45f9b4489a0c3f7cb6f0f8941736ae48767
TsubasaHayashi <yoku.rin.99@gmail.com>
2015-02-18 01:09:43 +0900

    フォームのデザイン変更

--- sp_form/www/js/app.js
+++ sp_form/www/js/app.js
@@ -3,52 +3,40 @@
   var module = angular.module('app', ['onsen']);
 
   module.controller('AppController', function($scope, $data) {
-    $scope.doSomething = function() {
-      setTimeout(function() {
-        alert('tappaed');
-      }, 100);
-    };
+
+
+
+
   });
 
-  module.controller('DetailController', function($scope, $data) {
-    $scope.item = $data.selectedItem;
+  module.controller('FormController', function($scope, $data) {
+    //
+
   });
 
   module.controller('HomeController', function($scope, $data) {
-    $scope.items = $data.items;
 
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.ons.navigator.pushPage('detail.html', {title : selectedItem.title});
+    /*
+    setCenter();
+    //画面の真ん中を取得する
+    function setCenter()
+    {
+      var windowHeight = $(window).height();
+      var centerValue = windowHeight / 2;
+
+      $data.centerValue = setCenter();
+    }
+    */
+
+    //formに遷移
+    $scope.go_to_form = function() {
+      $scope.ons.navigator.pushPage('form.html');//, {title : selectedItem.title});
     };
+
   });
 
   module.factory('$data', function() {
       var data = {};
-
-      data.items = [
-          {
-              title: 'sample1',
-              label: '4h',
-              desc: 'あるとき。。'
-          },
-          {
-              title: 'sample2',
-              label: '6h',
-              desc: 'ああああ'
-          },
-          {
-              title: 'sample3',
-              label: '1day ago',
-              desc: 'ああああ'
-          },
-          {
-              title: '4',
-              label: '1day ago',
-              desc: 'テスト'
-          }
-      ];
 
       return data;
   });
