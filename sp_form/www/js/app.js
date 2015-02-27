dc2b92a1cad96af4c8c763e8edf7c6f9c9975f4d
TsubasaHayashi <yoku.rin.99@gmail.com>
2015-02-27 11:12:07 +0900

    コミット

--- sp_form/www/js/app.js
+++ sp_form/www/js/app.js
@@ -8,6 +8,7 @@
 
   module.controller('HomeController', function($scope, $data) {
 
+    //$("ons-page").css("backgroundColor","red");
     /*
     setCenter();
     //画面の真ん中を取得する
@@ -29,65 +30,82 @@
   });
 
 
-  module.controller('FormController', function($scope, $data) {
+  module.controller('FormController', function($scope, $data, $location) {
 
-    $scope.items = [
-        {
-            question: '名前は？'
-            //answer: 'テスト'
-        }/*,
-        {
-            question: '性別は？',
-            answer: '男'
-        },
-        {
-            question: '誕生日は？',
-            answer: '2015/2/20'
-        },
-        {
-            question: '出身は？',
-            answer: '東京'
-        }*/
-    ];
+    //itemsにプロパティを追加
+    $scope.items = [ { question: 'お名前を教えてください' } ];
 
-    $scope.num = 0;
+    //$scope.num = 0;
 
-
+    /*
     var update = function(index) {
       $('#answer'+index).css('display','');
     };
+    */
+
+    //enterkey をおされたときの処理
+    /*$('').keypress( function ( e ) {
+      if ( e.which == 13 ) {
+        $scope.postText();
+      }
+    });*/
 
 
     $scope.postText = function(index)
     {
       var questionValue = [];
-      questionValue[0] = '名前は？';
-      questionValue[1] = '性別は？';
-      questionValue[2] = '出身は？';
-      questionValue[3] = 'アホなの？';
-      questionValue[4] = '頭いいの？';
-      questionValue[5] = 'エロいの？';
+      var next = index + 1;
+      questionValue[0] = 'お名前を教えてください';
+      questionValue[1] = '性別を教えてください';
+      questionValue[2] = '出身を教えてください';
+      questionValue[3] = '年齢を教えてください';
+      questionValue[4] = '住所を教えてください';
+      questionValue[5] = 'あああを教えてください';
 
       if(this.inputText)
       {
         //get input box value
         var textValue = this.inputText;
-        console.log("入力内容:"+ textValue);
+        console.log("回答"+index+":"+ textValue);
 
+        //回答者と回答を表示(jQuery未動作のため未実装　【原因】jQueryとangularjsの競合？)
         //$("#answer"+index).css("backgroundColor","red");
+        //$("#answer").addClass("");
+        //$("#answer"+index).addClass("displayon");
 
         //番号に応じた質問と答えをセット
         $scope.items[index] = ({ question: questionValue[index], answer: this.inputText });
-        $scope.items.push({ question: questionValue[index+1] });
+        $scope.items.push({ question: questionValue[next] });
+
+
+        //新しい質問部分に移動
+        var nextQuesLocation = 'question' + next;
+        //$location.absUrl() == "file:///Users/hayashi/Documents/develop/move_form/sp_form/www/index.html#"+nextQuesLocation
+        //$location.path() == '/'+ nextQuesLocation
+
+        $location.path(nextQuesLocation);
+        
+
+        /*
+        if($location.hash() != nextQuesLocation)
+        {
+          $location.hash('answer' + next);
+        }else{
+          $anchorScroll.yOffset = 50;
+        }
+        */
 
 
         $scope.inputText = "";　//ボックスのクリア
-        $scope.$apply();   //画面の更新
+
+        //$scope.$apply();   //画面の更新(itemの更新処理はなぜか自動で行われている)
       }else{
         alert("文字が入力されていません。");
       }
 
     };
+
+
 
   });
 
