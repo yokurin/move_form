1dae5770e3e44888de69c37898aff1786cec176a
TsubasaHayashi <yoku.rin.99@gmail.com>
2015-03-06 11:48:52 +0900

    アップロード画面中の画面追加

--- sp_form/www/js/app.js
+++ sp_form/www/js/app.js
@@ -52,6 +52,7 @@
     */
 
     var questionValue = [];
+    var answerValue = [];
     var errorMsg = [];
     //質問と回答の番号はセット（同じ）で回数を計測
     var allCnt = 0;
@@ -66,20 +67,27 @@
     //質問の設定
     questionValue[0] = '名前は？';
     questionValue[1] = '性別は？  男性は1 女性は2 を入力';
+    /*
     questionValue[2] = '出身は？';
     questionValue[3] = '年齢は？';
     questionValue[4] = '住所は？';
+    */
+    //questionValue[5] = '以上で終わりになります。ありがとうございました。';
+    /*
     for(var o=5; o<10; o++)
     {
-      questionValue[o] = '質問'+o+'の内容';
+      questionValue[o] = '質問'+o+'の内容  バリデーションは半角数字で設定';
     }
+    */
+
+
 
     //エラーメッセージの設定
     errorMsg[0] = "入力に誤りがあります。もう一度";
     errorMsg[1] = "入力に誤りがあるようですね。";
     errorMsg[2] = "質問にちゃんと答えましょう。";
     errorMsg[3] = "しっかりしてください。";
-    errorMsg[4] = "違う。日本語わかりますか。";
+    errorMsg[4] = "違います。日本語わかりますか。";
 
     //itemsにプロパティの設定
     $scope.items = [ { question: questionValue[0], answer: ''} ];
@@ -102,7 +110,7 @@
       {
         //get input box value
         var textValue = this.inputText;
-        console.log("回答"+ allCnt +":"+ textValue);
+        console.log("全ての回答["+ allCnt +"]："+ textValue);
 
         //回答者と回答を表示
         //(jQuery未動作のため未実装　【原因】jQueryとangularjsの競合？)
@@ -115,6 +123,8 @@
           //番号に応じた質問と答えをセット
           $scope.items[allCnt].answer =  textValue;
           $scope.items.push({ question: questionValue[successNext] });
+          //成功した答えを保存
+          answerValue[successCnt] = textValue;
 
           //成功時に実行
           success();
@@ -138,18 +148,32 @@
 
         //$scope.$apply();   //画面の更新(itemの更新処理はなぜか自動で行われている)
 
+        if( successCnt == questionValue.length)
+        {
+          postModal.show();
+          setTimeout('postModal.hide()', 1500);
+
+          setTimeout('ons.navigator.pushPage(thanks.html)', 1900);
+        }
+
+        //成功した答えを全て表示
+        for(var s=0; s < answerValue.length; s++)
+        {
+          console.log("正しい回答["+ s + "]：" + answerValue[s]);
+        }
+
       }else{
         alert("文字が入力されていません。");
       }
 
     };
 
-
-    function validation(index, text)
+    //バリデーション
+    function validation(nowNumber, text)
     {
 
       //名前のバリデーション
-      if( index == 0)
+      if( nowNumber == 0)
       {
         if( text.match(/^[亜-熙ぁ-んァ-ン]+$/) )
         {
@@ -158,7 +182,7 @@
       }
 
       //性別のバリデーション
-      if(index == 1)
+      if( nowNumber == 1)
       {
         if( text.match(/^[0-9]+$/) )
         {
@@ -167,7 +191,7 @@
       }
 
       //出身のバリデーション
-      if(index == 2)
+      if( nowNumber == 2)
       {
         if( text.match(/^[亜-熙ぁ-んァ-ン]+$/) )
         {
@@ -176,7 +200,7 @@
       }
 
       //年齢のバリデーション
-      if(index == 3)
+      if( nowNumber == 3)
       {
         if( text.match(/^[0-9]+$/) )
         {
@@ -185,7 +209,7 @@
       }
 
       //住所のバリデーション
-      if(index == 4)
+      if( nowNumber == 4)
       {
         if( text.match(/^[亜-熙ぁ-んァ-ン]+$/) )
         {
@@ -203,6 +227,7 @@
     {
       allCnt++;
       successCnt++;
+
     }
 
 
